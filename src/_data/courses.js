5fc0b471633652d9bc4259ff0d6704729bb32208
Nazmul Alam <nazmuldipu@gmail.com>
2021-06-19 18:28:38 +0600

    courses list added

---
+++ src/_data/courses.js
@@ -0,0 +1,31 @@
+module.exports = [{
+    "name": "Javascript",
+    "label": "Easy to learn",
+    "logo": "https://seeklogo.com/images/J/javascript-logo-8892AEFCAC-seeklogo.com.png",
+    "url": "/javascript"
+},
+{
+    "name": "Tailwind CSS",
+    "label": "Build whatever you wantn",
+    "logo": "https://cdn.worldvectorlogo.com/logos/tailwind-css-1.svg",
+    "url": "/tailwindcss"
+},
+{
+    "name": "Angular",
+    "label": "Develop across all platforms",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg",
+    "url": "/angular"
+},
+{
+    "name": "React",
+    "label": "Building user interfaces",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+    "url": "/react"
+},
+{
+    "name": "Nodejs",
+    "label": "Back-end JavaScript",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg",
+    "url": "/nodejs"
+}
+]
